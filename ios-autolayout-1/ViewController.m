4ad490939e7ccf4e9ff3561e17a2b1cd6104b0ca
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-18 10:12:56 +0900

    first commit

---
+++ ios-autolayout-1/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ios-autolayout-1
+//
+//  Created by Manami Ichikawa on 5/18/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
